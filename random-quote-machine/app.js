711fa5c973f175e00ab06407bc952b6796682f0e
Gilles Margerin <gillesmargerin@gmail.com>
2020-08-17 10:33:45 +0200

    basic html structure and components

--- random-quote-machine/app.js
+++ random-quote-machine/app.js
@@ -1,9 +1,54 @@
-class Home extends React.Component {
+class App extends React.Component {
   render() {
     return(
-      <div>Some text</div>
+      <div id='quote-box'>
+        <Text/>
+        <Author/>
+        <NewQuote/>
+        <Tweet/>
+      </div>
     )
   }
 }
 
-ReactDOM.render(<Home />, document.querySelector('#main'))
+class Text extends React.Component {
+  render() {
+    return(
+      <div id='text'>
+
+      </div>
+    )
+  }
+} 
+
+class Author extends React.Component {
+  render() {
+    return(
+      <div id='author'>
+
+      </div>
+    )
+  }
+}
+
+class NewQuote extends React.Component {
+  render() {
+    return(
+      <button id='new-quote'>
+
+      </button>
+    )
+  }
+}
+
+class Tweet extends React.Component  {
+  render() {
+    return(
+      <a id='tweet-quote'>
+
+      </a>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#main'))

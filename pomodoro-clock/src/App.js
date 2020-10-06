2c9289d366dabb45329083fa0733882dc87b095f
Gilles Margerin <gillesmargerin@gmail.com>
2020-10-06 14:59:03 +0200

    html structure

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import TypeWrap from './TypeWrap'
+import TimerWrap from './TimerWrap'
 
 const App = () => {
   return (
     <div className="App">
-      Textbox
+      <TimerWrap labelType='timer' timerType='25:00'/>
+      <div className='container'>
+        <TypeWrap labelType='session' timerType='25'/>
+        <TypeWrap labelType='break' timerType='5'/>
+      </div>
     </div>
   );
 }
---
+++ pomodoro-clock/src/TypeWrap.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TypeWrap = (props) => {
+  return(
+    <section id={`${props.labelType}-label`} className='wrapper'>
+      <label>{props.labelType}</label>
+      <p id={`${props.labelType}-length`}>{props.timerType}</p>      
+      <button type='button' id={`${props.labelType}-increment`}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M12.354 8.854l5.792 5.792a.5.5 0 01-.353.854H6.207a.5.5 0 01-.353-.854l5.792-5.792a.5.5 0 01.708 0z"></path></svg>
+      </button>
+      <button type='button' id={`${props.labelType}-decrement`}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M11.646 15.146L5.854 9.354a.5.5 0 01.353-.854h11.586a.5.5 0 01.353.854l-5.793 5.792a.5.5 0 01-.707 0z"></path></svg>
+      </button>
+    </section>
+  )
+}
+
+export default TypeWrap
